b0784043a2a1a46c87bd74f9738b69ba8e093c49
Moti1120083 <audit_7000@test.com>
2026-02-10 14:30:29 +0800

    fixed doc

--- pension_tool.py
+++ pension_tool.py
@@ -9,7 +9,7 @@
 # 1. Double check excel files are in CSV. This tool will not work for other file extensions
 # 2. Make sure excel files do not have unnecessary summation lines at the end
 #
-# How to:
+# How-to:
 # 1. Drop all the files from previous month (改服, 補發, 大宗, 新梯) into the 'resources' folder
 #    So for example, if i'm working on 11510 pensions, i'd include all the files from October, 115.
 # 2. Run 'python pension_tool.py' in command line and the results will be outputted to 'output' folder
